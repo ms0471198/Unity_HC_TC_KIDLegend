0db1d83ec73f79be031c0e123efcf75dcf5c2216
KID <50666207+KID421@users.noreply.github.com>
2019-12-18 20:59:16 +0800

    使用剛體推力讓角色移動

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Joystick joystick;
+    public Rigidbody rig;
+
+    public float speed = 10;
+
+    // 固定更新：一秒 50 次 - 控制物理
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        float v = joystick.Vertical;        // 垂直
+        float h = joystick.Horizontal;      // 水平
+
+        rig.AddForce(-h * speed, 0, -v * speed);    // 推力(-水平，0，-垂直)
+    }
+}
