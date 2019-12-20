9e562c02a110c1ba5e7b35e6d89fbba03b362ddb
KID <50666207+KID421@users.noreply.github.com>
2019-12-20 18:47:27 +0800

    控制跑步動畫

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -7,10 +7,12 @@ public class Player : MonoBehaviour
 
     private Joystick joystick;  // 虛擬搖桿類別
     private Rigidbody rig;      // 剛體
+    private Animator ani;       // 動畫控制器
 
     private void Start()
     {
         rig = GetComponent<Rigidbody>();                                    // 取得元件<泛型>() (取得相同屬性面板)
+        ani = GetComponent<Animator>();                                    // 取得元件<泛型>() (取得相同屬性面板)
         joystick = GameObject.Find("虛擬搖桿").GetComponent<Joystick>();     // 遊戲物件.尋找("物件名稱").取得元件<泛型>()
     }
 
@@ -29,5 +31,9 @@ public class Player : MonoBehaviour
         float h = joystick.Horizontal;              // 水平
 
         rig.AddForce(-h * speed, 0, -v * speed);    // 推力(-水平，0，-垂直)
+
+        // v 1、-1 h 1、-1 要打開跑步開關
+        // v 0 h 0 關閉
+        ani.SetBool("跑步開關", v != 0 || h != 0);
     }
 }
