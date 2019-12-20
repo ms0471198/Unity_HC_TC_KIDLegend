7875430c869ded76a59e8dedf330b7bcdc938962
KID <50666207+KID421@users.noreply.github.com>
2019-12-20 20:06:53 +0800

    攝影機定義欄位：玩家、速度與上方下方限制

--- UnityProject/Assets/Scripts/CameraControl.cs
+++ UnityProject/Assets/Scripts/CameraControl.cs
@@ -2,5 +2,14 @@
 
 public class CameraControl : MonoBehaviour
 {
+    #region 欄位
+    private Transform player;
 
+    [Header("跟蹤速度"), Range(0, 10)]
+    public float speed = 1.5f;
+    [Header("上方限制"), Tooltip("")]
+    public float top = -10;
+    [Header("下方限制"), Tooltip("")]
+    public float bottom = 5;
+    #endregion
 }
