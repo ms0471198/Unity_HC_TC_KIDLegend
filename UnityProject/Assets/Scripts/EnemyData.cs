9aa8f67eb3aea18c3d9ca7977775e3657a4ff111
KID <50666207+KID421@users.noreply.github.com>
2020-01-08 21:25:52 +0800

    建立腳本化物件：怪物資料

---
+++ UnityProject/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+// 腳本化物件 ScriptableObject：將此腳本的資料儲存為物件並放在 Project 內
+
+// 建立素材選項(檔案名稱 = ""，選項名稱 = "/")
+[CreateAssetMenu(fileName = "怪物資料", menuName = "KID/怪物")]
+public class EnemyData : ScriptableObject
+{
+    [Header("血量"), Range(10, 3000)]
+    public float hp;
+    [Header("攻擊力"), Range(0, 1000)]
+    public float attack;
+    [Header("攻擊冷卻時間"), Range(0, 5)]
+    public float cd;
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed;
+}
--- UnityProject/Assets/Scripts/LevelManager.cs
+++ UnityProject/Assets/Scripts/LevelManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;  // 引用 場景管理 API
 using System.Collections;
 
 public class LevelManager : MonoBehaviour
@@ -23,27 +24,47 @@ public class LevelManager : MonoBehaviour
         imgCross = GameObject.Find("轉場效果").GetComponent<Image>();
 
         if (autoShowSkill) ShowSkill();
-        if (autoOpenDool) Invoke("OpenDoor", 6);    // 延遲調用(方法，延遲時間)
+        if (autoOpenDool) Invoke("OpenDoor", 4.5f);    // 延遲調用(方法，延遲時間)
     }
 
+    /// <summary>
+    /// 顯示技能
+    /// </summary>
     private void ShowSkill()
     {
         randomSkill.SetActive(true);
     }
 
+    /// <summary>
+    /// 開門並顯示光照
+    /// </summary>
     private void OpenDoor()
     {
         objLight.SetActive(true);
         door.SetTrigger("開門觸發");
     }
 
+    /// <summary>
+    /// 載入下一關
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator NextLevel()
     {
-        print("載入下一關");
+        // 載入下一關
+        //SceneManager.LoadScene("場景名稱"); // 場景管理器.載入場景("場景名稱")
 
-        // 迴圈 for while
-        // 0.01 透明度 + 0.01
-        imgCross.color += new Color(1, 1, 1, 0.01f);
-        yield return new WaitForSeconds(0.01f);
+        AsyncOperation async = SceneManager.LoadSceneAsync("關卡2"); // 場景管理器.非同步載入場景("場景名稱")
+
+        // 不允許直接載入
+        async.allowSceneActivation = false;
+
+        for (int i = 0; i < 50; i++)                       // 迴圈 for while
+        {
+            imgCross.color += new Color(1, 1, 1, 0.02f);    // 0.01 透明度 + 0.01
+            yield return new WaitForSeconds(0.001f);        // 等待 0.01 秒
+        }
+
+        // 允許直接載入
+        async.allowSceneActivation = true;
     }
 }
